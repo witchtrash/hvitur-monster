0b533a733f60a3483515e7f12bf847a42c4b54c6
emilia <emilia@hakke.ro>
2022-11-17 15:04:25 +0000

    Meta stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Global, css } from '@emotion/react';
 import { Scene } from 'components/Scene';
+import Head from 'next/head';
 
 const styles = css`
   body {
@@ -10,6 +11,35 @@ const styles = css`
 
 const Index = () => (
   <main>
+    <Head>
+      <title>Hvítur Monster</title>
+
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Hvítur Monster" />
+      <meta name="twitter:image" content="/og.png" />
+
+      <meta property="og:title" content="Hvítur Monster" key="title" />
+    </Head>
     <Global styles={styles} />
     <Scene />
   </main>
