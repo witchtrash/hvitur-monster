e55a44b672aa035718a876ca6abb9b8cbd88bc0e
Emilía <emilia@hakke.ro>
2023-10-10 15:09:39 +0000

    Fix issue with HDR asset

--- src/components/MonsterCan.tsx
+++ src/components/MonsterCan.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { useLoader } from '@react-three/fiber';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { Sparkles } from '@react-three/drei';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 export const MonsterCan = () => {
-  const model = useLoader(GLTFLoader, '/assets/monster-can/can.glb');
+  const model = useLoader(GLTFLoader, '/monster_zero_ultra.glb');
 
   return (
     <React.Suspense>
